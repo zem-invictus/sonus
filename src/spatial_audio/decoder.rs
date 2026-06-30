313829cf3eeab5abc5f6c5594d72827e6f6c0fcc
vladimir <vovazms@gmail.com>
2026-06-30 17:22:40 +0300

    WIP

--- src/spatial_audio/decoder.rs
+++ src/spatial_audio/decoder.rs
@@ -1,3 +1,5 @@
+use super::biquad::{BiquadCoefficients, BiquadState};
+use super::control::{DecoderControl, PlaybackRegistration};
 use bevy::audio::Decodable;
 use bevy::prelude::{Asset, TypePath};
 use hound::{WavReader, WavSpec, WavWriter};
@@ -9,83 +11,6 @@ use std::sync::{Arc, mpsc};
 use std::time::Duration;
 
 type InnerDecoder = Decoder<Cursor<Arc<[u8]>>>;
-
-#[derive(Debug, Clone, Copy)]
-pub struct BiquadCoefficients {
-    b0: f32,
-    b1: f32,
-    b2: f32,
-    a1: f32,
-    a2: f32,
-}
-
-impl BiquadCoefficients {
-    pub fn low_pass(cutoff_hz: f32, sample_rate: f32, q: f32) -> Self {
-        let cutoff = cutoff_hz.clamp(20.0, sample_rate * 0.49);
-        let omega = 2.0 * std::f32::consts::PI * cutoff / sample_rate;
-        let cos_w = omega.cos();
-        let sin_w = omega.sin();
-        let alpha = sin_w / (2.0 * q);
-        let a0 = 1.0 + alpha;
-
-        Self {
-            b0: ((1.0 - cos_w) / 2.0) / a0,
-            b1: (1.0 - cos_w) / a0,
-            b2: ((1.0 - cos_w) / 2.0) / a0,
-            a1: (-2.0 * cos_w) / a0,
-            a2: (1.0 - alpha) / a0,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy, Default)]
-pub struct BiquadState {
-    s1: f32,
-    s2: f32,
-}
-
-impl BiquadState {
-    #[inline(always)]
-    pub fn process_sample(&mut self, input: f32, coeffs: &BiquadCoefficients) -> f32 {
-        let output = coeffs.b0 * input + self.s1;
-        self.s1 = coeffs.b1 * input - coeffs.a1 * output + self.s2;
-        self.s2 = coeffs.b2 * input - coeffs.a2 * output;
-        output
-    }
-}
-
-#[derive(Clone)]
-pub struct DecoderControl {
-    cutoff_hz: Arc<AtomicU32>,
-    volume: Arc<AtomicU32>,
-}
-
-impl DecoderControl {
-    pub fn new(cutoff_hz: f32, volume: f32) -> Self {
-        Self {
-            cutoff_hz: Arc::new(AtomicU32::new(cutoff_hz.to_bits())),
-            volume: Arc::new(AtomicU32::new(volume.to_bits())),
-        }
-    }
-    pub fn set_cutoff(&self, cutoff_hz: f32) {
-        self.cutoff_hz.store(cutoff_hz.to_bits(), Relaxed);
-    }
-    pub fn get_cutoff(&self) -> f32 {
-        f32::from_bits(self.cutoff_hz.load(Relaxed))
-    }
-
-    pub fn set_volume(&self, volume: f32) {
-        self.volume.store(volume.to_bits(), Relaxed);
-    }
-    pub fn get_volume(&self) -> f32 {
-        f32::from_bits(self.volume.load(Relaxed))
-    }
-}
-
-pub struct PlaybackRegistration {
-    pub playback_id: u64,
-    pub control: DecoderControl,
-}
 
 #[derive(Asset, TypePath, Clone)]
 pub struct SpatialAudioSource {
@@ -186,7 +111,8 @@ impl Iterator for SpatialDecoder {
 
         let channel = (self.sample_counter - 1) % (self.channels as usize);
 
-        let filtered_sample = self.channel_states[channel].process_sample(float_sample, &self.coeffs);
+        let filtered_sample =
+            self.channel_states[channel].process_sample(float_sample, &self.coeffs);
 
         Some(filtered_sample)
     }
